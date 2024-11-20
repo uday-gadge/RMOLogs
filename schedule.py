f2d9f431fec04d1bf475116d8c14d52f53cc3e07
uday-gadge <uday.gadge@atkinsrealis.com>
2024-11-20 10:49:16 -0700

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -11,7 +11,7 @@ import os
 import io
 import asyncio
 import time
-import mexl_dio_planning
+import UpdatingRMOLogs
 
 @flow(log_prints=True)
 def update_rmo_logs():
@@ -21,21 +21,20 @@ if __name__ == '__main__':
     with open(os.path.join(os.path.expanduser('~'), 'git_access_token.txt'), 'r') as f:
         git_token = f.read()
     block = GitHub(
-        repository="https://github.com/anup-ctio/mexl_dio_planning.git",
+        repository="https://github.com/uday-gadge/RMOLogs.git",
         access_token=git_token,
         reference='main'
     )
-    block.save("git-dio-planning", overwrite=True)
+    block.save("git-rmo-logs", overwrite=True)
     
-    git_storage = GitHub.load('git-dio-planning')
-    
-    scheduled_deployment_dio_dashboard = Deployment.build_from_flow(
-        flow=update_dio_dashboard,
-        name="mexl_dio_planning",
+    git_storage = GitHub.load('git-rmo-logs')
+    scheduled_deployment_rmo_logs = Deployment.build_from_flow(
+        flow=update_rmo_logs,
+        name="update_rmo_logs",
         storage=git_storage,
-        schedule=(CronSchedule(cron="0 7 * * 1", timezone="America/Denver")),
+        schedule=(CronSchedule(cron="0 4 * * *", timezone="America/Denver")),
         work_queue_name="default",
         work_pool_name="my-process-pool"
     )
 
-    scheduled_deployment_dio_dashboard.apply()
+    scheduled_deployment_rmo_logs.apply()
